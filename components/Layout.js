4f795899321a7c6ef9529179b75b2007ffcd2c22
Shubham <bakhalshubham@gmail.com>
2021-08-28 16:29:00 +0530

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,167 @@
+import React, { useContext } from 'react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+  Badge,
+  Button,
+  Menu,
+  MenuItem,
+  Box,
+  createTheme,
+} from '@material-ui/core';
+import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Layout({ title, description, children }) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart, userInfo } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const classes = useStyles();
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const loginClickHandler = e => {
+    setAnchorEl(e.currentTarget);
+  };
+  const loginMenuCloseHandler = (e, redirect) => {
+    setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
+  };
+  const logoutClickHandler = () => {
+    setAnchorEl(null);
+    dispatch({ type: 'USER_LOGOUT' });
+    Cookies.remove('userInfo');
+    Cookies.remove('cartItems');
+    Cookies.remove('shippinhAddress');
+    Cookies.remove('paymentMethod');
+    router.push('/');
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - Next.js Ecom` : 'Next.js Ecom'}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar className={classes.toolbar}>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography className={classes.brand}>
+                    Next.js Ecom
+                  </Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  <Typography component="span">
+                    {cart.cartItems.length > 0 ? (
+                      <Badge
+                        color="secondary"
+                        badgeContent={cart.cartItems.length}
+                      >
+                        Cart
+                      </Badge>
+                    ) : (
+                      'Cart'
+                    )}
+                  </Typography>
+                </Link>
+              </NextLink>
+              {userInfo ? (
+                <>
+                  <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={loginClickHandler}
+                    className={classes.navbarButton}
+                  >
+                    {userInfo.name}
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={loginMenuCloseHandler}
+                  >
+                    <MenuItem
+                      onClick={e => loginMenuCloseHandler(e, '/profile')}
+                    >
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={e => loginMenuCloseHandler(e, '/order-history')}
+                    >
+                      Order Hisotry
+                    </MenuItem>
+                    
+                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <NextLink href="/login" passHref>
+                  <Link>
+                    <Typography component="span">Login</Typography>
+                  </Link>
+                </NextLink>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next.js Ecom.</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { SnackbarProvider } from 'notistack';
+import { useEffect } from 'react';
+import '../styles/globals.css';
+import { StoreProvider } from '../utils/Store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // for removing serverside rendering of material ui elements
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+      <StoreProvider>
+        <PayPalScriptProvider deferLoading={true}>
+          <Component {...pageProps} />
+        </PayPalScriptProvider>
+      </StoreProvider>
+    </SnackbarProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
